9cf324de3907881cc95f1146c0de6ace3628e3c1
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-12-03 14:08:15 +0530

    added navbar header img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/Navbar";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import HeaderImage from "./assets/headerImage.svg";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#DF6B57",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <NavBar />
+        <img src={HeaderImage} />
+      </div>
+    </ThemeProvider>
   );
 }
 
