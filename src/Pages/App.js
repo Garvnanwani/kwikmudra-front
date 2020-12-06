cc34a0b512484368644d8a3f6fa0421af72bb121
Garvnanwani <garvnanwani88@gmail.com>
2020-12-06 17:50:17 +0530

    centered the layout

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,20 +1,22 @@
-import AboutSection from "../components/AboutSection";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import NavBar from "../components/Navbar";
-import ProductsPage from "../components/ProductsPage";
-import "../styles/App.css";
+import AboutSection from "../components/AboutSection"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import NavBar from "../components/Navbar"
+import ProductsPage from "../components/ProductsPage"
+import "../styles/App.css"
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Header />
-      <ProductsPage />
-      <AboutSection />
+      <div className="content">
+        <NavBar />
+        <Header />
+        <ProductsPage />
+        <AboutSection />
+      </div>
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
