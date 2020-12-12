5e6a3fc65ad4de770419076fc01f097a7a9d6e9e
Garvnanwani <garvnanwani88@gmail.com>
2020-12-12 20:08:58 +0530

    added data to 4 forms

--- src/components/products/HomeLoanBalanceTransfer.js
+++ src/components/products/HomeLoanBalanceTransfer.js
@@ -122,7 +122,96 @@ const HomeLoanBalanceTransfer = () => {
           Submit
         </Button>
       </form>
-      <Typography variant="pre"></Typography>
+      <div style={{ margin: "20px 0px" }}>
+        <Typography
+            variant="body2"
+            color="textSecondary"
+            align="center"
+            style={{ textAlign: "left" }}
+        >
+          Home Loan Balance Transfer
+          Try not to pass up on an opportunity to bring down your loan reimbursements when you have more better choices to look over. Move your Home Loan outstanding balances to us, pay lower monthly scheduled payments and enjoy the savings for different things in life that truly matter to you and your family.
+          Transfer your outstanding Home Loan availed from another Bank/Financial Institution to us and get an extra Top Up Loan.
+          •	Appealing interest rates that make your Home Loan moderate and easier on your pocket
+          •	Customized reimbursement alternatives to suit your requirements
+          •	No shrouded charges
+          •	Coordinated branch network for profiting and adjusting the loan anyplace in India
+          •	Special arrangement for Home Loans for those employed in the Indian Army.
+
+          Who can Apply? ‘’
+
+          Primary Applicant
+          •	AGE: 21-65 years
+          •	Salaried/Self Employed: NATIONALITY
+          •	Inhabitant Indian: Sex
+          •	All genders
+          Co-applicant(s)
+          •	Including Co-Applicant helps in expanding the loan total.
+          •	Including Women Co-Owner aides in getting better interest rates.
+          •	All co-applicants need not be co-proprietors. By and large co-applicants are family members.
+
+
+          What are the advantages of moving home advance?
+          •	Decrease in interest rate from high rate to as low as 6.95% per annum
+          •	Decrease in month to month EMI by up to 5% relying on the rate contrast, balance tenure and EMI
+          •	Alternative to avail top up advance at same rate as home loan rate (6.95% to 8.8%) subject to eligibility conditions and loan to value proportion
+          •	Lower interest rates and other discounts like nil processing charge offered by another lender
+          •	Alternative to opt for smart saver home credit facility or max gain facility to spare interest
+          •	Balance transfer alternatives are now available at different banks like SBI, Bank of Baroda, HDFC, ICICI and others
+
+
+          Key Features of Home Loan Balance Transfer
+
+          •	Move the outstanding balance of your existing home advance to another bank or from one moneylender to the next.
+
+          •	There is a charge normally equivalent to 1% of the loan transferred that is payable to the new moneylender for home loan by the borrower.
+
+          •	Most of the times, the home loan balance transfer application is dealt with like another home loan application.
+
+          •	The balance transfer on a current home advance must be availed after a pre-decided timespan as referenced on the original loan agreement.
+
+          •	At the point when the transfer is finished, the borrower owes the moved principal advance sum in addition to relevant charges to the new lender rather than the first one.
+
+          Documents Required for Home Loan Balance Transfer
+
+          Your home loan balance transfer is dealt with like a new home advance application by the bank you are moving the loan to. Along these lines, while applying for your home loan to transfer to another bank, all the documents provided during the initial home advance application should be resubmitted. These documents are then revalidated and vetted by the bank or NBFC giving the loan transfer facility. Documents are the most significant component while taking up a loan since they best assist the bank to identify the loan borrower to ensure their advance borrowing and advance reimbursement capacities. The essential key documents for home loan transfers are the following:
+
+          •	Passport-size photos
+
+          •	Totally filled application form for transfer issued by the financial organization.
+
+          •	Latest three months' Salary Slips demonstrating break up of Gross salary that is the Basic Pay, House lease and Net Salary after derivations, if any.
+
+          •	A half year's bank statement, reflecting salary credits updated within 15 days before the advance application.
+
+          •	Identity Proof (Any One): Pan Card, Passport, Driving License or Voter's ID card or employee identity card (as identity proof and signature proof in case of of government workers).
+
+          •	Proof of address
+
+          •	Proof of Age (Any One): 10th or 12th Marks Cards, PAN Card or Voters ID Card.
+
+          •	On the off chance that you are an independently employed personnel or businessman, then rather than Points (3.) and (4.), you need to provide documents demonstrating the existence of your business (for businesspersons) and scholastic capabilities (for experts) ; and budget reports for both.
+
+          •	Bank articulations from wherein the home loan EMIs were deducted adding up last 12 months of the account.
+
+          •	The Loan statement copy and complete set of documents relating to the property that is right now in possession of the current home credit lender.
+
+
+          How can I avail the best home loan transfer offer?
+
+          •	Check loan fee history of the new moneylender: You should check that the lower interest rate being publicized by the new loan lender is genuine and not a shot term trick. If it's not too much trouble approach your loan advisor for the benchmark track record of the new bank.
+
+          •	Satisfy yourself about service quality of the new loan specialist: Check that the service quality offered by the new bank you are picking is up to your desires. Lower rate ought not to come at the expense of substandard services.
+
+          •	Check the benchmark rate: There are two regularly utilized benchmark rates for home advances – MCLR rate in case of banks and prime loaning rate (PLR) in case of housing finance organizations. MCLR benchmarked loans are known to be more straightforward and consequently ideal over PLR benchmarked advances.
+
+          •	Is the spread variable or fixed: Interest rate on floating rate loans comprises of two sections – benchmark rate and spread above it. While the benchmark rate is expected to change after some time, the spread is supposed to stay consistent except in case of a default. Be that as it may, a few banks offer floating rate loan with both the benchmark and the spread being variable. In case of numerous such credits, borrowers see their loan interest rates rise sharply after a couple of months. Along these lines, maintain a strategic distance from loans with variable spreads and rather settle on floating rate loans that vary interest rate only with change in the benchmark rate.
+
+          •	Estimate transaction cost: Check the cost that you will acquire for affecting the change. These incorporate processing fees, stamp duty (in certain states like Maharashtra) and documentation charges.
+
+          •	Issue notice to existing bank: A few banks demand a prior notice before you can prepay your home loan. Check your loan agreement cautiously and ensure that due notice is given to or waived by your current bank.
+       </Typography >
+      </div >
       <Copyright />
     </>
   )
--- src/components/products/TermInsurance.js
+++ src/components/products/TermInsurance.js
@@ -122,7 +122,97 @@ const TermInsurance = () => {
           Submit
         </Button>
       </form>
-      <Typography variant="pre"></Typography>
+      <div style={{ margin: "20px 0px" }}>
+        <Typography
+            variant="body2"
+            color="textSecondary"
+            align="center"
+            style={{ textAlign: "left" }}
+        >
+          What is Term Insurance?
+          Term insurance plan or policy is a kind of life insurance where the insurer gives coverage for a specific 'term' in return for a particular premium paid over a stretch of time. Whenever insured dies during the term period indicated in the policy, a death benefit is paid to the group of the insured.
+          Why do I need Term Insurance?
+          To take care of my family
+          If you are an earning individual of the family, then you must have term insurance. When you decide to purchase term insurance, you will get genuine feelings of serenity in realizing that your income will be replaced if that you are no longer there. In the event of your sad end, the advantages from the term plan can help your friends and family pay for their standard costs and furthermore accomplish their long-term objectives.
+          To take care of my credits
+          If that you have any huge liabilities like home/car/personal loans then you must purchase a term plan. At the point when you purchase, if it's not too much trouble guarantee that the life cover amount you select is multiple times your yearly pay PLUS the total value of considerable liabilities you have.
+          To make sure about my wellbeing
+          Our term plan provides enhanced protection against many perilous sicknesses including Cancer, Heart Attack, Kidney failure, Stroke, Alzheimer's Disease, Parkinson's Disease and Encephalitis.
+          Benefits of a term insurance plan
+          Affordability
+          Term insurance plans are a blend of affordability with simplicity. You find something useful to do a life cover of 50L at premiums as low as 17/day*
+
+
+          Tax breaks
+          You can profit tax breaks under segment 80C of the Income Tax Act, 1961, for the premium paid and entirety guaranteed of your term protection plan.
+          Simple to Buy
+          Rather than stepping out of your home, you are now able to buy term insurance plans online with a couple of clicks, according to your convenience.
+          Claim Payouts can be Staggered
+          Staggered monthly payouts give an alternative to claim a lump-sum amount and the rest of the claim as Monthly Income forestalling any misuse of the total cover.
+          Flexibility in Premium Payments
+          You have the alternative to pay premiums on a month to month, semi-annual, or yearly basis. Additionally, there are likewise term insurance plans that offer single pay premium alternative.
+          Return of Sum Assured
+          Term Insurance Plan's Dual Protect include restores your Sum assured after retirement.
+          Extra Riders with your Plan
+          You can include riders like accidental death rider, disability rider, critical illness rider, income benefit rider, waiver of premium rider, and so on for another shielding layer.
+          Insurance Against Liabilities
+          During our lifetime, we purchase a vehicle, house, and so on. On the off chance that an mishap occurs to you - accident, disability, or demise - the term protection plan replaces your monetary role in the family.
+          Features of a Term Insurance Policy
+          Save Tax U/S 80C and 80D
+
+          The premium you pay for your term protection plan can save tax for you. Critical illness premium spares tax for you u/s 80D, while life insurance premium is checked u/s 80C of Income Tax, subject to conditions
+
+          Long Policy Term
+          You can pick a more extended policy term to remain secured. You can remain covered for to 50 years with a term life cover.
+
+          Low Entry Age
+          Most term Plans have a base entry age of 18 years; that is, you can purchase term insurance when you hit adult age.
+
+          Death Benefit as Regular Income
+          Present day term insurance plans permit you to give your dependents a regular income alongside the lump-sum benefit in case of your unfortunate death.
+
+          Premiums Returned on Survival
+          You now are able to get all your money back if you survive the policy term. Term insurance plans with the arrival of premium benefit offer to return all the paid premiums on the policy as survival benefit.
+
+          Add Disability Benefit to Your Term Plan
+          Any sort of disability because of accident or illness can affect your income earning limit. You can include disability benefit with your term plan at affordable prices. The disability spread is available on payment of extra premium.
+
+          Cover Against Life Threatening Diseases
+          Cancer, heart attack, renal failure are some curable ailments which can be perilous. Try not to let money concern you while in transit to the cure from these diseases. Critical illness cover is accessible on payment of extra premium
+
+
+
+
+          High Maturity Age
+          Of all life insurance plans, term insurance offers the life cover for the highest noteworthy age bracket. You can proceed with the term-insurance cover up to the age of 85.
+
+          Term insurance riders-Must-have Add-ons
+
+          What are term insurance riders?
+          A rider is an extra or a paid component to your base term plan. Each rider intends to overhaul the extent of your basic policy coverage. Be that as it may, what advantages do riders bring? Is it accurate to say that are worth the extra money? Do they really give complementary coverage? If you are gazing at a list of riders and pondering whether to consider them, how about we take a gander at some essential add-on benefits that can improve your term plan and help you choose.
+          1. Accidental death rider
+          Indian roads saw more than 1.46 lakh mishaps during the year 2017. This is a number that can't be ignored.
+          Most ideal precautionary measures are possible but there isn't a lot of you can do if the other driver commits a mistake. That is the reason numerous individuals include an accidental death benefit rider in their term policy.
+          This implies that if an insured individual passes away because of an accident, the accidental death rider pays an aggregate well beyond the basic total guaranteed to the nominee.
+          2. Income benefit rider
+          As the name suggests, an income benefit rider guarantees your family gets a consistent flow of income even in your absence.
+          The income received supplements the sum assured guaranteed to your beneficiaries/nominees. For instance, by opting for the income benefit rider, your family would get an extra 10% of the sum assured for a specific number of years.
+          The fundamental motivation behind the rider is to generate income for your family. The ideal time for this rider can extend somewhere in the range of 5-10 years.
+          3. Waiver of premium rider
+          When you become a term insurance policy holder, you need to guarantee all of your payments are made on time. In any case, imagine a scenario in which you can't proceed with regular payments because of a critical illness. Generally, the term insurance company drops the term policy.
+          For somebody who has made the premium payments normally on schedule, this can come as a genuine blow. Be that as it may, there is an approach to avoid this situation. On the off chance that the policyholder is diagnosed with any of the 36 critical illnesses, a 'waiver of premium rider' guarantees all your future premiums get waived off.
+          It is a low-cost choice to keep your policy active regardless of whether you can't make your payments.
+          4. Comprehensive Critical Illness Care Rider
+          The Rider gives single amount benefit and waiver of premium benefit in case of first event of any of the covered critical diseases conditions during the lifetime of the insured and gave the incident happening during the coverage time frame.
+
+          Why should you purchase this term insurance plan?
+          o	Monetary security of your family if there should arise an occurrence of your absence.
+          o	Providing standard month to month pay to your family post your earning years or if there should be an occurrence of your deplorable passing;
+          o	Possibility of your money related liabilities turning into a weight to your family in case of eventualities.
+          o	Dangers of Accident and Terminal Illness, and their subsequent budgetary impact.
+
+        </Typography>
+      </div>
       <Copyright />
     </>
   )
