da182326f26e9724668753f752aecc2a1c55c822
Garvnanwani <garvnanwani88@gmail.com>
2020-12-12 18:01:13 +0530

    modified form component

--- src/Routing.js
+++ src/Routing.js
@@ -4,9 +4,9 @@ import NotFound from "./components/NotFound"
 import AboutPage from "./Pages/AboutPage"
 import Dashboard from "./Pages/Admin/Dashboard"
 import App from "./Pages/App"
+import ProdForm from "./Pages/ProdForm"
 import SignIn from "./Pages/Signin"
 import Signup from "./Pages/Signup"
-import TempForm from "./Pages/TempForm"
 import "./styles/index.css"
 
 const Routing = () => {
@@ -18,7 +18,7 @@ const Routing = () => {
         <Route path="/signup" component={Signup} />
         <Route path="/about" component={AboutPage} />
         <Route path="/admin" component={Dashboard} />
-        <Route path="/product/:productName" component={TempForm} />
+        <Route path="/product/:productName" component={ProdForm} />
         <Route component={NotFound} />
       </Switch>
     </Router>
