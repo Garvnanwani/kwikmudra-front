eaa7539bf3b74eb7dc93f1dadf717356a6720de4
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-12-12 19:34:23 +0530

    edited products

--- src/components/products.js
+++ src/components/products.js
@@ -1,16 +1,16 @@
 const products = [
-  {
-    title: "Life Insurance",
-    desc: `Life Insurance is an agreement between an insurer and a policyholder in which the insurer ensures payment of a death benefit to named beneficiaries when the insured dies.`,
-    link: "/product/lifeinsurance",
-    name: "lifeinsurance",
-  },
-  {
-    title: "Home Loan",
-    desc: `A home loan is an amount of money that an individual borrows from a bank or money lending agency at a specific rate of interest to be paid with the EMI monthly.`,
-    link: "/product/homeloan",
-    name: "homeloan",
-  },
+  // {
+  //   title: "Free Credit Score",
+  //   desc: "Check, track & build your credit score for best loan & card offers",
+  //   link: "/product/freecreditscore",
+  //   name: "freecreditscore",
+  // },
+  // {
+  //   title: "Credit Cards",
+  //   desc: "From 50+ options, choose a card matching your lifestyle & needs",
+  //   link: "/product/creditcards",
+  //   name: "creditcards",
+  // },
   {
     title: "Personal Loan",
     desc:
@@ -18,17 +18,29 @@ const products = [
     link: "/product/personalloan",
     name: "personalloan",
   },
+  // {
+  //   title: "Micro Loans (Under ₹50k)",
+  //   desc: "Instant small ticket loans to meet your immediate cash needs",
+  //   link: "/product/microlens",
+  //   name: "microlens",
+  // },
   {
     title: "Business Loan",
-    desc: "Instant small ticket loans to meet your immediate cash needs",
+    desc: "Expand your business with loans at lowest interest rates",
     link: "/product/businessloan",
     name: "businessloan",
   },
+  // {
+  //   title: "Transfer Personal Loan",
+  //   desc: "Get better interest rates on your existing personal loan",
+  //   link: "/product/transferpersonalloan",
+  //   name: "transferpersonalloan",
+  // },
   {
-    title: "Home Loan Balance Transfer",
-    desc: "Expand your business with loans at lowest interest rates",
-    link: "/product/homeloanbalancetransfer",
-    name: "homeloanbalancetransfer",
+    title: "Home Loan",
+    desc: "Compare & choose from lowest interest rates for your dream home",
+    link: "/product/homeloan",
+    name: "homeloan",
   },
   {
     title: "Term Insurance",
@@ -36,6 +48,18 @@ const products = [
     link: "/product/terminsurance",
     name: "terminsurance",
   },
+  {
+    title: "Home Loan Balance Transfer",
+    desc: "Expand your business with loans at lowest interest rates",
+    link: "/product/homeloanbalancetransfer",
+    name: "homeloanbalancetransfer",
+  },
+  // {
+  //   title: "Transfer Home Loan",
+  //   desc: "Save on your home loan by transferring it to lower interest rates",
+  //   link: "/product/transferhomeloan",
+  //   name: "transferhomeloan",
+  // },
   {
     title: "Loan Against Property",
     desc: "Get liquidity against your property at great interest rates",
@@ -49,10 +73,46 @@ const products = [
     name: "goldloan",
   },
   {
+    title: "Life Insurance",
+    desc: "Get insurance for your loved ones & secure their future",
+    link: "/product/lifeinsurance",
+    name: "lifeinsurance",
+  },
+  {
     title: "Health Insurance",
     desc: "Protect yourself & your family in a hour of need",
     link: "/product/healthinsurance",
     name: "healthinsurance",
   },
-]
-export default products
+  // {
+  //   title: "Savings Account",
+  //   desc: "Explore the best ways to make your money grow",
+  //   link: "/product/savingsaccount",
+  //   name: "savingsaccount",
+  // },
+  // {
+  //   title: "Fixed Deposits",
+  //   desc: "For secured returns on your investments",
+  //   link: "/product/fixeddeposits",
+  //   name: "fixeddeposits",
+  // },
+  // {
+  //   title: "Direct Mutual Funds",
+  //   desc: "For secured returns on your investments",
+  //   link: "/product/directmutualfunds",
+  //   name: "directmutualfunds",
+  // },
+  // {
+  //   title: "Guaranteed Return Plan",
+  //   desc: "Insurance to secure your family’s future",
+  //   link: "/product/guaranteedreturnplan",
+  //   name: "guaranteedreturnplan",
+  // },
+  // {
+  //   title: "Free DEMAT Account",
+  //   desc: "Start trading in stocks & currencies",
+  //   link: "/product/freedemantaccount",
+  //   name: "freedemantaccount",
+  // },
+];
+export default products;
