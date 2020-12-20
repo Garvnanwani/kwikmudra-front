9ce514794f3f4185b91418aeba007dc441cfc220
Shubh Agrawal <shubhagrawal1280@gmail.com>
2020-12-20 15:46:35 +0530

    added employee signup route

--- src/Routing.js
+++ src/Routing.js
@@ -1,13 +1,14 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import NotFound from "./components/NotFound"
-import AboutPage from "./Pages/AboutPage"
-import Dashboard from "./Pages/Admin/Dashboard"
-import App from "./Pages/App"
-import ProdForm from "./Pages/ProdForm"
-import SignIn from "./Pages/Signin"
-import Signup from "./Pages/Signup"
-import "./styles/index.css"
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import AboutPage from "./Pages/AboutPage";
+import Dashboard from "./Pages/Admin/Dashboard";
+import App from "./Pages/App";
+import ProdForm from "./Pages/ProdForm";
+import SignIn from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import EmployeeSignUp from "./Pages/EmployeeSignup";
+import "./styles/index.css";
 
 const Routing = () => {
   return (
@@ -17,12 +18,13 @@ const Routing = () => {
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={Signup} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/admin" component={Dashboard} />
+        <Route path="/admin/employee/signup" component={EmployeeSignUp} />
+        <Route path="/admin" exact component={Dashboard} />
         <Route path="/product/:productName" component={ProdForm} />
         <Route component={NotFound} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
